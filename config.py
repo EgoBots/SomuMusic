b83ac3320c65bf1def4fbf0e50fab8bc0e4e6d27
×𝐄𝐆𝐎//𝐑𝐄𝐌𝐈𝐗/𝟑 <Shizubots@outlook.com>
2025-08-06 11:47:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mrtosumemon/somumusic",
+    "https://github.com/EgoBots/SomuMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/The_Incricible")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Zoyu_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AarumiBots")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AarumiChat")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -117,3 +117,4 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
